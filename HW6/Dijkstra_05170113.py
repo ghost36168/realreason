960b47a395ba25f3e28609182cfd14149df216e0
ghost36168 <55727099+ghost36168@users.noreply.github.com>
2020-01-02 14:41:01 +0800

    Add files via upload

---
+++ HW6/Dijkstra_05170113.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from collections import defaultdict
+
+class Graph():
+    
+    def __init__(self,vertices):
+        self.V =vertices
+        self.graph = []
+        self.graph_matrix =[[0 for column in range(vertices)]
+                           for row in range(vertices)]
+    def addEdge(self,u,v,w):
+        
+    def Dihkstra(self,s):
+        
+    def Kruskal(self):
+
